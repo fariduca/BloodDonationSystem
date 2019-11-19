ceb069c6b4aacf2969573bb264c8481f28a5a972
Faridun <faridun.mamadbekov_2021@ucentralasia.org>
2019-11-19 15:21:30 +0600

    Add project files.

---
+++ Blood Donation System/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blood_Donation_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blood_Donation_System.Models.DonorsModel> DonorsModel { get; set; }
+    }
+}
---
+++ Blood Donation System/Models/DonorsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blood_Donation_System.Models
+{
+    public class DonorsModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone_Number { get; set; }
+
+        public string Blood_Type { get; set; }
+
+    }
+}
