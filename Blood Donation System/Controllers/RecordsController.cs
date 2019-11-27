c8a9fbbf14ab8ea4d8c5bd4349d0c6249beb8561
Faridun <faridun.mamadbekov_2021@ucentralasia.org>
2019-11-27 18:54:11 +0600

    faridun more edits

---
+++ Blood Donation System/Controllers/RecordsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blood_Donation_System.Data;
+using Blood_Donation_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blood_Donation_System.Controllers
+{
+    public class RecordsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecordsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: Records
+        public async Task<IActionResult> Index()
+        {
+            List<DonorsModel> records = await _context.DonorsModel.ToListAsync();
+            return View(records);
+        }
+
+        // GET: Records/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.DonorsModel.FindAsync(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            return View(record);
+        }
+
+        // GET: Records/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Records/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Phone_Number,Blood_Type")] DonorsModel records)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(records);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Records/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var record = await _context.DonorsModel.FindAsync(id);
+            if (record == null)
+            {
+                return NotFound();
+            }
+
+            return View(record);
+        }
+
+        // POST: Records/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Records/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Records/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
