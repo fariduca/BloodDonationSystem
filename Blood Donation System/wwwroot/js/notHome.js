55e3150c622cf34ed83d7944973e8af06cb3c0a1
Faridun <faridun.mamadbekov_2021@ucentralasia.org>
2019-12-05 19:24:17 +0600

    almost final commit

--- Blood Donation System/wwwroot/js/notHome.js
+++ Blood Donation System/wwwroot/js/notHome.js
@@ -1,7 +1,3 @@
 ﻿var aboutLink = document.getElementById('aboutLink');
 var donateLink = document.getElementById('donateLink');
 var contactLink = document.getElementById('contactLink');
-
-aboutLink.href = '/#about-section';
-donateLink.href = '/#';
-contactLink.href = '/#';
